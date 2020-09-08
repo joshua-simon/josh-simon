4c88efbcd6a1d7d7e5ae1dc4941e08f68a21abcf
Joshua-Simon <joshy.d.simon@gmail.com>
2020-09-08 13:35:33 +1000

    homepage layout complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import './styles.css'
+import Homepage from './components/Homepage'
 
 function App() {
   return (
-<div>test</div>
+<div>
+<Homepage/>
+</div>
   );
 }
 
---
+++ src/components/Homepage.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import josh from '../Photos/josh.png'
+
+const Homepage = () => {
+    return (
+        <div>
+        <>
+        <div>
+            <h1>Joshua Simon / Web Developer</h1>
+            <h2>/ Projects</h2>
+        <div className = 'hashigo'>
+            <div>
+           <h2 className = 'number'>1/</h2><h2 className = 'project-name'>Hashigo Zake Website</h2>
+           <a href = 'https://hashigo-zake.herokuapp.com'>
+           <h3 className = 'links'>Website</h3>
+           </a>
+           <a href = 'https://github.com/joshua-simon/hashigo-zake-v2/'>
+           <h3 className = 'links'>Code</h3>
+           </a>
+           </div>
+        </div>
+        <div className = 'chore-share'>
+            <div>
+           <h2 className = 'number'>2/</h2><h2 className = 'project-name'>Chore Share (in progress)</h2>
+           <a href = 'https://chore-share.herokuapp.com/'>
+           <h3 className = 'links'>Website</h3>
+           </a>
+           <a href = 'https://github.com/joshua-simon/chore-share-v2'>
+           <h3 className = 'links'>Code</h3>
+           </a>
+           </div>
+        </div>
+        <div className = 'fml'>
+            <div>
+           <h2 className = 'number'>3/</h2><h2 className = 'project-name'>Follow My Lead</h2>
+           <a href = 'https://f-m-l.herokuapp.com'>
+           <h3 className = 'links'>Website</h3>
+           </a>
+           <a href = 'https://github.com/joshua-simon/hashigo-zake-v2/'>
+           <h3 className = 'links'>Code</h3>
+           </a>
+           </div>
+        </div>
+        </div>
+        <img src = {josh}></img>
+        </>
+        </div>
+    )
+}
+
+export default Homepage
